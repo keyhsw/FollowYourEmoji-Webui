7a5d7f0093217ed4864f1ee45fdb58c7eecb2e90
key <30455795+keyhsw@users.noreply.github.com>
2024-07-30 14:16:08 +0800

    Create download_models_openxlab.py

---
+++ download_models_openxlab.py
@@ -0,0 +1,18 @@
+import os
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+emoji_path = './ckpt_models/'
+create_directory(emoji_path)          
+os.system(f'git clone https://code.openxlab.org.cn/houshaowei/FollowYourEmoji_BaseModelPack.git {base_path}')
+os.system(f'cd {emoji_path} && git lfs pull')   
+
+
+base_path = './ckpt_models/base'
+create_directory(base_path)          
+os.system(f'git clone https://code.openxlab.org.cn/houshaowei/FollowYourEmoji_BaseModelPack.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')    
+
+    
